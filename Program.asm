9387d92b9b6061b6df7cbcbed376f533aa2fc4c6
ShinDasha <d.shyn@ukma.edu.ua>
2024-03-22 12:46:15 +0200

    changed the method for reading the file

--- Program.asm
+++ Program.asm
@@ -1,40 +1,60 @@
-
-
 .model small
 .stack 100h
 
 .data
-    charArray db 1000 dup(0) 
-
+    dispMsg db 'Input:$'
+    lenDispMsg equ $-dispMsg
+    buffer db 255 dup(?)  ; Buffer to store the input string
+    oneChar db 0 
+    filename  db "test.in", 0
+    mesBad db "File error $"
+    handle dw 0
 .code
-start PROC
-    mov ax, @data
+start: 
+mov ax, @data
     mov ds, ax
 
-  
-    mov cx, 1000 
-    lea bx, charArray 
-    readChars:
-        mov ah, 01h 
-        int 21h 
-        mov [bx], al 
-        inc bx 
-        loop readChars 
+    mov dx, offset fileName; Address filename with ds:dx 
+    mov ah, 03Dh ;DOS Open-File function number 
+    mov  al, 0;  0 = Read-only access 
+    int 21h; Call DOS to open file 
 
+    jc error ;Call routine to handle errors
+        jmp cont
+    error:
+        mov ah, 09h
+    mov dx, offset mesBad
+    int 21h
+    jmp ending
+    cont:
 
+    mov [handle] , ax ; Save file handle for later
+
+;read file and put characters into buffer
+read_next:
+    mov ah, 3Fh
+    mov bx, [handle]  ; file handle
+    mov cx, 1   ; 1 byte to read
+    mov dx, offset oneChar   ; read to ds:dx 
+    int 21h   ;  ax = number of bytes read
+    ; do something with [oneChar]
+
+    ;save ax
+    push ax
+    push bx
+    push cx
+    push dx
+    mov ah, 02h
+    mov dl, oneChar
+    int 21h
    
-    mov cx, 1000
-    lea bx, charArray 
-    writeChars:
-        mov dl, [bx] 
-        mov ah, 02h 
-        int 21h 
-        inc bx 
-        loop writeChars 
+pop dx
+pop cx
+pop bx
+pop ax
+    or ax,ax
+    jnz read_next
 
-   
-    mov ax, 4c00h 
-    int 21h 
-start ENDP
+ending:
 
-END start
+end start
