cd80229ecdd88956f16e1148115112620e77c377
ShinDasha <d.shyn@ukma.edu.ua>
2024-03-27 23:42:17 +0200

    added a method for clearing registers and bubble sort

--- v2.asm
+++ v2.asm
@@ -2,24 +2,25 @@
 .stack 100h
 
 .data
-oneChar db 0 
-numbers dw 10 dup(2)
-arrayIndex dw 0
-countt db 0
-power dw 0
-inputBuffer dw 0
-separate db 0
-valueNegative db 0
-sumL dw 0
-sumH dw 0
-сWords dw 0
+oneChar db 0 ; змінна для зберігання символу
+numbersArray dw 10 dup(2) ; масив чисел
+powerCounter dw 0         ; лічильник ступеня десятки
+inputValue dw 0           ; буфер для зберігання числа
+arrayIndex dw 0           ; індекс для масиву
+digitCount db 0           ; лічильник введених цифр
+spaceCount db 0           ; лічильник пробілів
+isNegative db 0           ; прапорець для визначення, чи від'ємне число введено
+sumLow dw 0               ; нижнє слово для суми
+sumHigh dw 0              ; верхнє слово для суми
+wordCount dw 0            ; лічильник слів
+
    
 .code
     main proc 
         mov ax, @data
         mov ds, ax 
         
-        ; call methods
+        ;call methods
         call input
         call bubbleSort
         call calculateMed
@@ -33,7 +34,7 @@ sumH dw 0
        inc arrayIndex
        inc arrayIndex
        inc сWords
-       
+       ret
     addToArray endp
 
     powerOfTen proc
@@ -49,109 +50,11 @@ sumH dw 0
         loop powerLoop
 
         endPowerOfTen:
-        
+        ret 
     powerOfTen endp
 
     input proc
-       inputStart:
-         mov ah,3Fh
-         mov bx, 0h 
-         mov cx, 1
-         mov dx, offset oneChar
-         int 21h
-
-         or ax,ax
-         jz skip
-
-         mov inputBuffer, ax
-
-         mov ah, 02h
-         mov dl, oneChar
-         int 21h
-
-         cmp oneChar, '-'
-         jne sCheck
-         mov valueNegative, 1
-         jmp inputStart
-
-      sCheck:
-         cmp oneChar, 0ah
-         je popCharacters 
-         cmp oneChar, 0dh
-         je popCharacters 
-         cmp oneChar, 20h
-         je popCharacters 
-
-         mov separate, 0
-
-         push dx
-         inc countt
-         jmp inputEnd
-
-      skip:
-         jmp popCharacters
-
-      inputEnd:
-         mov ax, inputBuffer
-         or ax, ax
-         jnz inputStart
-
-         cmp countt, 0
-         jne progJump ; pcJump
-         jmp calculationSrart
-        
-      progJump:
-         jmp popCharacters
-
-      popCharacters:
-         inc separate
-         cmp separate, 2 
-         jne popLoopFirst ; popLoopStart
-         jmp calculationSrart
-
-      popLoopFirst:
-         mov cl, countt
-         xor ax, ax 
-         xor dx, dx 
-
-      popLoop:
-         pop ax
-         sub ax, '0'
-
-         push cx
-         push dx 
-         call powerOfTen
-         pop dx
-         pop cx 
-
-         add dx, ax
-
-         inc power
-
-         loop popLoop
-
-       popLoopEnd:
-         mov countt, 0
-         mov power, 0
-
-         call floor
-         cmp valueNegative, 1
-         jne sum
-         neg dx
-
-      sum:
-        call addToArray
-         
-         mov sumH, 0
-         add sumL, dx 
-         adc sumH, 0
-
-         mov dx, 0
-         mov valueNegative, 0
-
-         jmp inputEnd
-         
-
+     
     input endp
 
     floor proc
@@ -160,7 +63,7 @@ sumH dw 0
       mov dx, 32767
 
       endFloor:
-      
+      ret
          
     floor endp
         
@@ -184,75 +87,30 @@ sumH dw 0
        loop innerLoop
        pop cx
        loop outerLoop
+
+      ret
     bubbleSort endp
 
-    clearRegist proc 
+    clearRegist proc ;обнулення регістрів
        xor ax,ax
        xor bx, bx
        xor cx, cx 
        xor dx, dx
        xor si, si 
-       
+       ret
     clearRegist endp
 
     calculation proc 
-      calculationSrart:
-        mov ah, 02h
-        mov dl, "g"
-        int 21h 
-        
+     
     calculation endp 
 
     calculateMed proc
-       call clearRegist
-
-       mov bx, сWords
-       mov ax, bx
-       and ax, 1
-       jz evenAmount
-       jnz oddAmound
-
-       evenAmount:
-       shr bx, 1
-       lea si, numbers
-
-       mov dx, [si+bx]
-       add dx, [si+bx-2]
-
-       jmp medianEnd
-
-       oddAmound:
-       shr bx, 1
-       inc bx
-       lea si, numbers
-
-       mov dx, [si+bx]
-
-       medianEnd:
-       add dx, "0"
-       mov ah, 02h
-       int 21h
-
       
     calculateMed endp
 
     calculateAv proc
-       call clearRegist
-
-       mov dx, sumH
-       mov ax, sumL
-       mov bx, сWords
-
-      cwd 
-      idiv bx
-      mov dx, ax
-      
-      add dx, "0"
-      mov ah, 02h
-      int 21h
-     
+       
     calculateAv endp
-  
     mov ax, 4c00h      ; DOS exit function
     int 21h            ; Exit program
    end main
