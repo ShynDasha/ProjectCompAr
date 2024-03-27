8c415bc9bd6959632f88e907e6175cc45096f8bb
ShinDasha <d.shyn@ukma.edu.ua>
2024-03-27 18:39:35 +0200

    the beginning of the complete program code

--- v2.asm
+++ v2.asm
@@ -1,26 +1,71 @@
-.model tiny
-.code   
-ORG 0100h
-start:
+.model small
+.stack 100h
+
+.data
+power dw 0
+iBuffer dw 0
+isSpace db 0
+isNegative db 0
+sumL dw 0
+sumH dw 0
+totalWords dw 0
+   
+.code
+    main proc 
+        mov ax, @data
+        mov dx, ax
+        ; виклик методів
+        call inputElem 
+        call bubbleSort
+        call calculateMed
+        call calculateAv
+
+    main endp
+
+    addToArray proc
+
+    addToArray endp
+
+    powerOfTen ;description
+    powerOfTen PROC
+      
+    powerOfTen ENDP
+
+    inputElem proc
+    
+    inputElem endp
+    
+    bubbleSort proc
+       mov cx, word ptr count
+       dec cx  ; count-1
+      outerLoop:
+       push cx
+       lea si, array
+      innerLoop:
+    mov ax, [si]
+    cmp ax, [si+2]
+    jl nextStep
+    xchg [si+2], ax
+    mov [si], ax
+nextStep:
+    add si, 2
+    loop innerLoop
+    pop cx
+    loop outerLoop
 
 
-    mov ah, 02h
-    mov dl, '2'
-    int 21h
-    mov dl, 13
-    int 21h
-    mov dl, 10
-    int 21h
-    mov dl, '4'
-    int 21h
-    mov dl, 13
-    int 21h
-    mov dl, 10
-    int 21h
+    bubbleSort endp
 
-    mov ax, 4C00h
-    int 21h
+    calculateMed proc
+    calculateMed endp
+
+    calculateAv proc
+    calculateAv endp
+
+    
 
 
-end start
+    
 
+
+    end 
